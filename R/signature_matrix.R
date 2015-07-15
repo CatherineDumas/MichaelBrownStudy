96db05e4f0de2c7b9f86ed0a0f48a6ac0566bbc7
William May <williamcmay@live.com>
2015-07-14 23:28:57 -0400

    Added market basket-format csv

--- R/signature_matrix.R
+++ R/signature_matrix.R
@@ -5,11 +5,12 @@
 
 
 # put your username and password here:
-user = "username"
-password = "password"
+user = "root"
+password = "root"
 
 # the list of petitions
-petitions = read.csv("MBS_Episode_1.txt", stringsAsFactors = F)
+petitions = as.vector(read.csv("MBS_Episode_1.txt", header = F,
+    stringsAsFactors = F)[,1 ])
 
 
 
@@ -17,34 +18,72 @@ petitions = read.csv("MBS_Episode_1.txt", stringsAsFactors = F)
 library(RMySQL)
 
 con = dbConnect(MySQL(), dbname = "wtp", user = user,
-    password = password, host = "127.0.0.1", port = 8888)
+    password = password)
 
-query = paste0("select * from signature where petition_id in ('",
-    paste(petitions[, 1], collapse = "', '"), "')")
+query = paste0("select * from wtp_data_signatures where petition_id in ('",
+    paste(petitions, collapse = "', '"), "')")
 signatures = dbGetQuery(con, query)
 
 dbDisconnect(con)
+
+# double checking
+pettab = table(signatures$petition_id)
 
 
 
 # cleaning the data
 
 # remove signatures with no zip code
-signatures = signatures[signatures$zip != "", ]
+signatures2 = signatures[grepl("^\\d{5}$", signatures$zip) &
+                           grepl("^[a-zA-Z]{2}$", signatures$name), ]
 
 # combine the name and zip code
-signatures$signer = paste(signatures$name, signatures$zip, sep = ", ")
+signatures2$signer = paste(toupper(signatures2$name),
+    signatures2$zip, sep = ", ")
 
 # get the total number of petitions signed by each unique signer
-totals = table(signatures$signer)
+totals = table(signatures2$signer)
 
-# Some of these numbers are totally bogus, with supposedly 150
-# petitions signed. I'm removing those.
-real_totals = totals[totals < 100]
+# the data's good now, this is no longer required
+## # Some of these numbers are totally bogus, with supposedly 150
+## # petitions signed. I'm removing those.
+## real_totals = totals[totals < 100]
 
-# the non-bogus signatures
-signatures = signatures[signatures$signer %in% names(real_totals), ]
+## # the non-bogus signatures
+## signatures = signatures[signatures$signer %in% names(real_totals), ]
 
+
+# write individual petition files and remove duplicated signatures
+setwd("../data") # move to the data folder
+signatures2$pet_id = match(signatures2$petition_id, petitions)
+for (petition in petitions) {
+  signatures3 = signatures2[signatures2$petition_id == petition, ]
+  tab3 = table(signatures3$signer)
+  signatures4 =
+    signatures3[signatures3$signer %in% names(tab3)[tab3 == 1], ]
+  write.csv(signatures4[, c("pet_id", "signer")],
+            file = paste0(petition, ".csv"), row.names = F)
+}
+
+
+# write file in market basket csv format
+library(reshape2)
+file_name = "gun_control_basket.csv"
+# convert from long to wide/matrix format
+sig_mat = dcast(signatures2, signer ~ pet_id)
+# get rid of the known duplicates-- check every row for an entry > 1
+sig_mat2 = sig_mat[apply(sig_mat, 1,
+    function(x) !any(as.integer(x[-1]) > 1)), ]
+pet_ids = names(sig_mat)[-1]
+# don't want to add to an existing file
+if (file.exists(file_name)) file.remove(file_name)
+for (n in 1:nrow(sig_mat2)) {
+  # list of petitions where the matrix entry is not zero
+  signed = paste(pet_ids[as.logical(sig_mat2[n, -1])],
+      collapse = ", ")
+  txt = paste0(sig_mat2$signer[n], ", ", signed, "\n")
+  cat(txt, file = file_name, append = T)
+}
 
 
 
